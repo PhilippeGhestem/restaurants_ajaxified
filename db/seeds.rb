ea470f44f42d73a8a91d1924b855d1097ac63d6d
Philippe Ghestem <philippe_ghestem@hotmail.com>
2021-02-28 22:26:08 +0100

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+10.times do
+  Restaurant.create(
+    name: Faker::Name.unique.last_name,
+    address: ["Paris", "Lyon", "Nantes", "Lille", "Nice", "Rennes", "Strasbourg", "Nice", "Toulouse", "Bordeaux", "Biarritz", "Brest", "Pau"].sample,
+    )
+end
+
+20.times do
+  Review.create(
+    content: Faker::Lorem.paragraph(sentence_count: 3, supplemental: true),
+    restaurant_id: (1..10).to_a.sample
+    )
+end
